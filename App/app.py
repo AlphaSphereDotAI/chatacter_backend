c44cf2ee484529d597d5e3adf6f61c1f65041aa6
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-08-02 01:39:28 +0300

    chore: Refactor app.py and settings.py

--- App/app.py
+++ App/app.py
@@ -1,35 +1,38 @@
 import requests
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse, FileResponse
 from chatacter.model import get_response
-from chatacter.settings import get_settings
+from chatacter.settings import Settings, get_settings
+from fastapi import FastAPI
+from fastapi.responses import FileResponse, JSONResponse
 
 app = FastAPI(debug=True)
-settings = get_settings()
+settings: Settings = get_settings()
 
 
-@app.get("/")
-async def is_alive():
+@app.get(path="/")
+async def is_alive() -> dict[str, str]:
     return {"message": "Chatacter is alive!", "status": "ok"}
 
 
-@app.get("/get_settings")
-async def get_settings():
+@app.get(path="/get_settings")
+async def get_settings() -> dict[str, str]:
     return settings.model_dump()
 
 
-@app.get("/get_text")
-def get_text(query: str, character: str):
-    res, time = get_response(query, character)
+@app.get(path="/get_text")
+def get_text(query: str, character: str) -> JSONResponse:
+    res, time = get_response(query=query, character=character)
     return JSONResponse(
-        content=res, headers={"time": time},
+        content=res,
+        headers={"time": time},
     )
 
 
-@app.get("/get_audio")
-def get_audio(text: str):
-    response_audio = requests.get(f"{settings.host.voice_generator}get_audio?text={text}")
-    with open(settings.assets.audio, "wb") as f:
+@app.get(path="/get_audio")
+def get_audio(text: str) -> FileResponse:
+    response_audio: requests.Response = requests.get(
+        url=f"{settings.host.voice_generator}get_audio?text={text}"
+    )
+    with open(file=settings.assets.audio, mode="wb") as f:
         f.write(response_audio.content)
     return FileResponse(
         path=settings.assets.audio,
@@ -38,18 +41,31 @@ def get_audio(text: str):
     )
 
 
-@app.get("/get_video")
-def get_video(character: str):
-    send_audio = requests.post(f"{settings.host.video_generator}set_audio", files={"file": open(settings.assets.audio, "rb")})
-    response_video = requests.get(f"{settings.host.video_generator}get_video?character={character}")
-    with open(settings.assets.video, "wb") as f:
+@app.get(path="/get_video")
+def get_video(character: str) -> FileResponse:
+    send_audio: requests.Response = requests.post(
+        url=f"{settings.host.video_generator}set_audio",
+        files={"file": open(file=settings.assets.audio, mode="rb")},
+    )
+    print("Send Audio: ", send_audio.status_code == 200)
+    response_video: requests.Response = requests.get(
+        url=f"{settings.host.video_generator}get_video?character={character}"
+    )
+    with open(file=settings.assets.video, mode="wb") as f:
         f.write(response_video.content)
     return FileResponse(
-        settings.assets.video, media_type="video/mp4",
-        filename="VIDEO.mp4", )
+        path=settings.assets.video,
+        media_type="video/mp4",
+        filename="VIDEO.mp4",
+    )
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app:app", host="localhost", port=8000, reload=True, )
+    uvicorn.run(
+        app="app:app",
+        host="localhost",
+        port=8000,
+        reload=True,
+    )
